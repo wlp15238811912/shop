f2b6c1719c8ef6227cfb137d405fa5f145fb1798
wanglipan <1662880176@qq.com>
2022-09-16 09:36:54 +0800

    新建Main文件提交

---
+++ src/components/main.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+<h1>main</h1>
+    </div>
+</template>
+<script>
+export default {
+   data() {
+      return {
+      }
+   },
+   created(){
+   },
+   computed:{
+   },
+   methods:{
+   },
+}
+</script>
+<style lang="scss" scoped>
+
+</style>
