f4167f20128058eafe527eb1c7c9971b5c03760c
wanglipan <1662880176@qq.com>
2022-09-19 09:29:23 +0800

    hello页面提交

---
+++ src/components/hello.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+<h1>hello页面</h1>
+    </div>
+</template>
+<script>
+export default {
+   data() {
+      return {
+      }
+   },
+   created(){
+   },
+   computed:{
+   },
+   methods:{
+   },
+}
+</script>
+<style lang="scss" scoped>
+
+</style>
